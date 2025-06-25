bb08f7a3fd17d84c18cbdaa97f5179b8c2ff0745
christo790 <christosgeorge0@gmail.com>
2025-06-25 08:03:11 +0530

    card app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World', style: TextStyle(fontSize: 24)),
+        backgroundColor: Colors.deepPurple,
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.all(20),
+            height: 100,
+            width: 100,
+            color: Colors.white,
+            child: Center(child: Text('data')),
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
