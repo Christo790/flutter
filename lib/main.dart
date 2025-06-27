6a16aafbe6575523e1b05070cba5c7309a05ee54
christo790 <christosgeorge0@gmail.com>
2025-06-27 07:37:48 +0530

    xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -9,35 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 70),
-                height: 100,
-                color: Colors.white,
-
-                child: Center(child: Text('data')),
-              ),
-              SizedBox(height: 30),
-              Container(
-                height: 100,
-                color: Colors.red,
-                child: Center(child: Text('data')),
-              ),
-              Container(
-                height: 100,
-                color: Colors.blue,
-                child: Center(child: Text('data')),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: Scaffold(backgroundColor: Colors.black));
   }
 }
